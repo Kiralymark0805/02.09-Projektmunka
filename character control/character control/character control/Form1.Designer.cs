8704ed5163ebeaecc3b0acfb4ac7f5163519bc5a
Mark <Mark@DESKTOP-AFI7DUF>
2023-02-14 21:02:47 +0100

    karakter mozgása, enemy mozgása és kulcsok spawnolása

---
+++ character control/character control/character control/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace character_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.player = new System.Windows.Forms.PictureBox();
+            this.playertimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.player.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("player.BackgroundImage")));
+            this.player.Location = new System.Drawing.Point(748, 211);
+            this.player.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(133, 123);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // playertimer
+            // 
+            this.playertimer.Enabled = true;
+            this.playertimer.Interval = 20;
+            this.playertimer.Tick += new System.EventHandler(this.movetimerevent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1576, 804);
+            this.Controls.Add(this.player);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(1594, 851);
+            this.MinimumSize = new System.Drawing.Size(1594, 851);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydownpress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyuppress);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer playertimer;
+    }
+}
+
---
+++ character control/character control/character control/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace character_control
+{
+    public partial class Form1 : Form
+    {
+
+        Random Randomnumber = new Random();
+
+        List<PictureBox> enemylist = new List<PictureBox>();
+        List<PictureBox> keylist = new List<PictureBox>();
+
+        bool Moveup, Movedown, Moveleft, Moveright;
+
+        int playerspeed = 10;
+        int enemyspeed = 1;
+
+        int enemycount = 2;
+        int keycount = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void movetimerevent(object sender, EventArgs e)
+        {
+
+            Playermovement();
+            Enemymovement();
+            Gameover();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Makeenemies();
+            MakeKeys();
+        }
+
+        private void keydownpress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                Moveleft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                Moveright = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                Moveup = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                Movedown = true;
+            }
+        }
+
+        private void keyuppress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                Moveleft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                Moveright = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                Moveup = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                Movedown = false;
+            }
+        }
+
+        private void Makeenemies()
+        {
+            while (enemylist.Count != enemycount)
+            {
+
+                PictureBox enemy = new PictureBox();
+                enemy.Width = 20;
+                enemy.Height = 20;
+                enemy.BackColor = Color.Red;
+                enemy.Tag = "enemy";
+
+                int enemyx = Randomnumber.Next(this.ClientSize.Width - enemy.Width);
+                int enemyy = Randomnumber.Next(this.ClientSize.Height - enemy.Height);
+
+                enemy.Location = new Point(enemyx, enemyy);
+
+                enemylist.Add(enemy); 
+                this.Controls.Add(enemy);
+            }
+
+        }
+
+        private void MakeKeys()
+        {
+            while (keylist.Count != keycount)
+            {
+
+                PictureBox key = new PictureBox();
+                key.Width = 20;
+                key.Height = 20;
+                key.BackColor = Color.Green;
+                key.Tag = "keys";
+
+                int keyx = Randomnumber.Next(this.ClientSize.Width - key.Width);
+                int keyy = Randomnumber.Next(this.ClientSize.Height - key.Height);
+
+                key.Location = new Point(keyx, keyy);
+
+                keylist.Add(key);
+                this.Controls.Add(key);
+            }
+        }
+
+        private void Playermovement()
+        {
+            if (Moveleft == true && player.Left > 0)
+            {
+                player.Left -= playerspeed;
+            }
+            if (Moveright == true && player.Left < 1080)
+            {
+                player.Left += playerspeed;
+            }
+            if (Moveup == true && player.Top > 0)
+            {
+                player.Top -= playerspeed;
+            }
+            if (Movedown == true && player.Top < 561)
+            {
+                player.Top += playerspeed;
+            }
+        }
+
+        private void Enemymovement()
+        {
+            foreach (Control enemies in this.Controls)
+            {
+                if ((string)enemies.Tag == "enemy")
+                {
+                    if (enemies.Left > player.Left)
+                    {
+                        enemies.Left -= enemyspeed;
+                    }
+
+                    if (enemies.Left < player.Left)
+                    {
+                        enemies.Left += enemyspeed;
+                    }
+
+                    if (enemies.Top > player.Top)
+                    {
+                        enemies.Top -= enemyspeed;
+                    }
+
+                    if (enemies.Top < player.Top)
+                    {
+                        enemies.Top += enemyspeed;
+                    }
+                }
+            }
+        }
+
+        private void Gameover()
+        {
+            foreach (PictureBox enemy in enemylist)
+            {
+                if (player.Bounds.IntersectsWith(enemy.Bounds))
+                {
+                    this.Controls.Remove(enemy);
+                }
+            }
+        }
+    }
+}
