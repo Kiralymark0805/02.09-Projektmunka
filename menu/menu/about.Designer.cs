2eb6ccd331732854c937fc253f340de73eeec04f
Reha Máté <82642616+RehaMate15@users.noreply.github.com>
2023-02-21 20:25:01 +0100

    Menu majdnem vegleges
    Kesz van az osszes menupont, meg lehet rajta alakitani.
    
    meg kell még csinálni a more information menut

---
+++ menu/menu/about.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace menu
+{
+    partial class about
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(about));
+            this.backbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backbutton
+            // 
+            this.backbutton.Location = new System.Drawing.Point(23, 323);
+            this.backbutton.Name = "backbutton";
+            this.backbutton.Size = new System.Drawing.Size(101, 42);
+            this.backbutton.TabIndex = 1;
+            this.backbutton.Text = "Back";
+            this.backbutton.UseVisualStyleBackColor = true;
+            this.backbutton.Click += new System.EventHandler(this.backbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(151, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(597, 120);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // about
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 387);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.backbutton);
+            this.Name = "about";
+            this.Text = "about";
+            this.Load += new System.EventHandler(this.about_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backbutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ menu/menu/menu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menu
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+
+        private void menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            playmenu pmenu = new playmenu();
+            pmenu.ShowDialog();
+            this.Close();
+        }
+
+        private void controlbutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            controls cmenu = new controls();
+            cmenu.ShowDialog();
+            this.Close();
+        }
+
+        private void aboutbutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            about amenu = new about();
+            amenu.ShowDialog();
+            this.Close();
+        }
+
+        private void exitbutton_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+    }
+}
