b39445ac441b84584ebe47cd93c2df31d6357d4f
Kiralymark0805 <79969484+Kiralymark0805@users.noreply.github.com>
2023-02-22 20:08:28 +0100

    összerakott játék (még nem végleges)

---
+++ Formjatek/menu/jatek.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace menu
+{
+    public partial class jatek : Form
+    {
+
+        Random Randomnumber = new Random();
+
+        List<PictureBox> enemylist = new List<PictureBox>();
+        List<PictureBox> keylist = new List<PictureBox>();
+        List<Panel> panellist = new List<Panel>();
+
+        bool Moveup, Movedown, Moveleft, Moveright;
+
+        int playerspeed = 10;
+        int enemyspeed = 1;
+
+        int enemycount = 2;
+
+        int keycount = 5;
+        int pickedupkeys = 0;
+
+        bool win = false;
+
+        public jatek()
+        {
+            InitializeComponent();
+        }
+
+        private void movetimerevent(object sender, EventArgs e)
+        {
+
+            Playermovement();
+            Enemymovement();
+            Walls();
+            Gameover();
+        }
+
+        private void jatek_Load(object sender, EventArgs e)
+        {
+            Makeenemies();
+            MakeKeys();
+        }
+
+        private void keydownpress(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left)
+            {
+                Moveleft = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                Moveright = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                Moveup = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                Movedown = true;
+            }
+
+            foreach (PictureBox key in keylist)
+            {
+                if (player.Bounds.IntersectsWith(key.Bounds))
+                {
+                    if (e.KeyCode == Keys.F)
+                    { 
+                        pickedupkeys++;
+                        keytext.Text = "kulcsok száma: " + pickedupkeys.ToString();
+                        house.Controls.Remove(key);
+                    }
+                    
+                }
+            }
+
+        }
+
+        private void keyuppress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                Moveleft = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                Moveright = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                Moveup = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                Movedown = false;
+            }
+        }
+
+        private void Walls()
+        {
+            panellist.Add(panel1);
+            panellist.Add(panel2);
+            panellist.Add(panel3);
+            panellist.Add(panel4);
+            panellist.Add(panel5);
+            panellist.Add(panel6);
+            panellist.Add(panel7);
+            panellist.Add(panel8);
+            panellist.Add(panel9);
+            panellist.Add(panel10);
+            panellist.Add(panel11);
+            panellist.Add(panel12);
+            panellist.Add(panel13);
+            panellist.Add(panel14);
+            panellist.Add(panel15);
+            panellist.Add(panel16);
+            panellist.Add(panel17);
+            panellist.Add(panel18);
+            panellist.Add(panel19);
+            panellist.Add(panel20);
+            panellist.Add(panel21);
+        }
+
+        private void Makeenemies()
+        {
+            while (enemylist.Count != enemycount)
+            {
+
+                PictureBox enemy = new PictureBox();
+                enemy.Width = 20;
+                enemy.Height = 20;
+                enemy.BackColor = Color.Red;
+                enemy.Tag = "enemy";
+
+                int enemyx = Randomnumber.Next(this.house.Width - enemy.Width);
+                int enemyy = Randomnumber.Next(this.house.Height - enemy.Height);
+
+                enemy.Location = new Point(enemyx, enemyy);
+
+                enemylist.Add(enemy);
+                house.Controls.Add(enemy);
+
+            }
+        }
+
+        private void MakeKeys()
+        {
+            while (keylist.Count != keycount)
+            {
+
+
+                PictureBox key = new PictureBox();
+                key.Width = 20;
+                key.Height = 20;
+                key.BackColor = Color.Green;
+                key.Tag = "keys";
+
+                int keyx = Randomnumber.Next(this.house.Width - key.Width);
+                int keyy = Randomnumber.Next(this.house.Height - key.Height);
+
+                key.Location = new Point(keyx, keyy);
+                keylist.Add(key);
+                house.Controls.Add(key);
+            }
+        }
+
+        private void Playermovement()
+        {
+            player.Tag = "player";
+            house.Controls.Add(player);
+
+            PictureBox move = new PictureBox();
+            move.Width = player.Width;
+            move.Height = player.Height;
+            move.Left = player.Left;
+            move.Top = player.Top;
+
+            bool canmove = true;
+
+            if (Moveleft)
+            {
+                move.Left -= playerspeed;
+            }
+
+            if (Moveright)
+            {
+                move.Left += playerspeed;
+            }
+
+            if (Moveup)
+            {
+                move.Top -= playerspeed;
+            }
+
+            if (Movedown)
+            {
+                move.Top += playerspeed;
+            }
+
+            foreach (Panel wall in panellist)
+            {
+
+                if (move.Bounds.IntersectsWith(wall.Bounds))
+                {
+
+                    canmove = false;
+
+                }
+            }
+
+            if (canmove)
+            {
+
+                player.Left = move.Left;
+                player.Top = move.Top;
+
+            }
+        }
+
+
+        private void Enemymovement()
+        {
+            foreach (Control enemies in house.Controls)
+            {
+                if ((string)enemies.Tag == "enemy")
+                {
+                    if (enemies.Left > player.Left)
+                    {
+                        enemies.Left -= enemyspeed;
+                    }
+
+                    if (enemies.Left < player.Left)
+                    {
+                        enemies.Left += enemyspeed;
+                    }
+
+                    if (enemies.Top > player.Top)
+                    {
+                        enemies.Top -= enemyspeed;
+                    }
+
+                    if (enemies.Top < player.Top)
+                    {
+                        enemies.Top += enemyspeed;
+                    }
+                }
+            }
+        }
+
+        private void Gameover()
+        {
+            foreach (PictureBox enemy in enemylist)
+            {
+                if (player.Bounds.IntersectsWith(enemy.Bounds))
+                {
+                    house.Controls.Remove(enemy);
+                    enemylist.Clear();
+                    
+                    this.Hide();
+                    playmenu menu = new playmenu();
+                    menu.ShowDialog();
+                    this.Close();
+
+                    
+                }
+
+            }
+
+            foreach (PictureBox key in keylist)
+            {
+
+                if (pickedupkeys == keycount)
+                {
+                    PictureBox winplace = new PictureBox();
+                    winplace.Width = 50;
+                    winplace.Height = 50;
+                    winplace.Location = new Point(100, 0);
+                    winplace.BackColor = Color.Black;
+                    winplace.Tag = "winplace";
+
+                    house.Controls.Add(winplace);
+
+                    if (player.Bounds.IntersectsWith(winplace.Bounds))
+                    {
+                        win = true;
+
+                        foreach (PictureBox enemy in enemylist)
+                        {
+                            if (win == true)
+                            {
+                                house.Controls.Remove(enemy);
+
+                                //itt nyer a játékos, ha akarsz átdobhat egy win oldalra és timerrel megoldhatod hogy egy idő után dobjon vissza a menübe
+
+                                /*
+                                Ezeről a hibákról tudok denem tudok vele mit kezdeni mostmár:
+                                Ha a játékos meghal és viszadob a playmenube, ha bezárod a formot akkor hibaüzenetet dob mert megváltozott az enemylist hossza és azzal nincs kedvem foglalkozni mostmár
+                                ha rámész egy kulcsra és felveszed azután is tudsz af-el kulcsokat felvenni.
+                                ezekkel nem nagyon tudokmostmár mit csinálni.
+                                A kulcsok betudnak spawnolni a panelek alá.
+
+                                A játéknál ha változtatni akarsz ezeket tudod.
+                                minden változó a tetején.
+
+                                enemycount = az enemyk darabszáma.
+                                keycount = kulcsok száma.
+                                enemy, playerspeed = sebesség;
+                                 */
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Formjatek/menu/menu.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace menu
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
+            this.button2 = new System.Windows.Forms.Button();
+            this.controlbutton = new System.Windows.Forms.Button();
+            this.aboutbutton = new System.Windows.Forms.Button();
+            this.exitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(458, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 55);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // controlbutton
+            // 
+            this.controlbutton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("controlbutton.BackgroundImage")));
+            this.controlbutton.Location = new System.Drawing.Point(458, 90);
+            this.controlbutton.Name = "controlbutton";
+            this.controlbutton.Size = new System.Drawing.Size(159, 55);
+            this.controlbutton.TabIndex = 2;
+            this.controlbutton.UseVisualStyleBackColor = true;
+            this.controlbutton.Click += new System.EventHandler(this.controlbutton_Click);
+            // 
+            // aboutbutton
+            // 
+            this.aboutbutton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("aboutbutton.BackgroundImage")));
+            this.aboutbutton.Location = new System.Drawing.Point(458, 151);
+            this.aboutbutton.Name = "aboutbutton";
+            this.aboutbutton.Size = new System.Drawing.Size(159, 62);
+            this.aboutbutton.TabIndex = 3;
+            this.aboutbutton.UseVisualStyleBackColor = true;
+            this.aboutbutton.Click += new System.EventHandler(this.aboutbutton_Click);
+            // 
+            // exitbutton
+            // 
+            this.exitbutton.Image = ((System.Drawing.Image)(resources.GetObject("exitbutton.Image")));
+            this.exitbutton.Location = new System.Drawing.Point(458, 219);
+            this.exitbutton.Name = "exitbutton";
+            this.exitbutton.Size = new System.Drawing.Size(159, 55);
+            this.exitbutton.TabIndex = 4;
+            this.exitbutton.UseVisualStyleBackColor = true;
+            this.exitbutton.Click += new System.EventHandler(this.exitbutton_Click);
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(640, 399);
+            this.Controls.Add(this.exitbutton);
+            this.Controls.Add(this.aboutbutton);
+            this.Controls.Add(this.controlbutton);
+            this.Controls.Add(this.button2);
+            this.Name = "menu";
+            this.Text = "menu";
+            this.Load += new System.EventHandler(this.menu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button controlbutton;
+        private System.Windows.Forms.Button aboutbutton;
+        private System.Windows.Forms.Button exitbutton;
+    }
+}
